26a1fdd5a481bd544d6bfbf23aa6028bdfe28b71
Dominique Clark <melaydominique@gmail.com>
2023-05-11 17:01:31 -0400

    home page styling, links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      
+        <h1 id='name'>Dominique Clark</h1>
+        <h2 id='title'>Software Engineer</h2>
+      <div id='brand'>
+        <h4 id='brandstatement'>I have a passion for solving difficult problems and writing elegant code.</h4>
+      </div>
+      <img id='headshot' src={process.env.PUBLIC_URL + '/images/headshot.png'} alt="Headshot" />
+      <div id='icons'>
+        <a href='https://github.com/dommelay' target='_blank'>
+          <img className='icon' src={process.env.PUBLIC_URL + '/images/github.png' } alt='icon'/>
         </a>
-      </header>
+        <a href='mailto:melaydominique@gmail.com'>
+          <img className='icon' src={process.env.PUBLIC_URL + '/images/gmail.png' } alt='icon'/>
+        </a>
+        <a href='https://www.linkedin.com/in/dominiquemelayclark/' target='_blank'>
+          <img className='icon' src={process.env.PUBLIC_URL + '/images/linkedin.png' } alt='icon'/>
+        </a>
+        <a href='https://twitter.com/domsoftwaredev?lang=en' target='_blank'>
+          <img className='icon' src={process.env.PUBLIC_URL + '/images/twitter.png' } alt='icon'/>
+        </a>
+      </div>
+      <img className='icon downarrow' src={process.env.PUBLIC_URL + '/images/down-arrow.png' } alt='icon'/>
     </div>
   );
 }
