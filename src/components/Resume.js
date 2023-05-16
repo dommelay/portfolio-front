5ff080456febd9f54033557ca839a510711934d0
Dominique Clark <melaydominique@gmail.com>
2023-05-16 14:03:39 -0400

    styling resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,13 +8,29 @@ const Resume = () => {
     return (
 
 
-        <div>
-            <a href='https://drive.google.com/file/d/1YmI9GWPQFnfqK_zgzeeki3P09NPfhw2w/view?usp=sharing' target='_blank' download>
-                <button>Download Resume</button>
-            </a>
-            <div id='test'>
-                <p>Hi</p>
+        <div id='resumecontainer'>
+
+            <div id='resumedownload'>
+                <a href='https://drive.google.com/file/d/1YmI9GWPQFnfqK_zgzeeki3P09NPfhw2w/view?usp=sharing' target='_blank' download>
+                    <button id='resumebtn'>Download Resume</button>
+                </a>
             </div>
+
+            <div id='educontainer'>
+                <div>
+                    <div className='schoollogo'>
+                        <h1 className='school'>University of Michigan</h1>
+                        <img className='edulogo' src='/images/umich.png' alt='university of michigan logo'/>
+                    </div>
+                    <div>
+                        <h3 className='edulocation'>Ann Arbor, MI</h3>
+                    </div>
+                </div>
+                <div>
+                    <img className='galogo' src='/images/ga.png' alt='general assembly logo'/>
+                </div>
+            </div>
+
         </div>
     )
 }
