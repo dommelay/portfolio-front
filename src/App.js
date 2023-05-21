e6f437defb192b2ed9bce9363f00be60c01752a1
Dominique Clark <melaydominique@gmail.com>
2023-05-20 21:18:53 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -17,13 +17,9 @@ const App = () => {
 <>
 
     <div className="App">
-      
-        <h1 id='name'>Dominique Clark</h1>
-        <h2 id='title'>Software Engineer</h2>
-      <div id='brand'>
-        <h4 id='brandstatement'>I have a passion for solving difficult problems and writing elegant code.</h4>
-      </div>
-      <img id='headshot' src={process.env.PUBLIC_URL + '/images/headshot.png'} alt="Headshot" />
+
+<div id='homecontainer'>
+
       <div id='icons'>
         <a href='https://github.com/dommelay' target='_blank'>
           <img className='icon' src={process.env.PUBLIC_URL + '/images/github.png' } alt='icon'/>
@@ -38,7 +34,24 @@ const App = () => {
           <img className='icon' src={process.env.PUBLIC_URL + '/images/twitter.png' } alt='icon'/>
         </a>
       </div>
+
+      <div id='headshotdiv'>
+          <img id='headshot' src={process.env.PUBLIC_URL + '/images/headshot.png'} alt="Headshot" />
+      </div>
+
+      <div id='titlediv'>
+        <h1 id='name'>Dominique Clark</h1>
+        <h2 id='title'>Software Engineer</h2>
+     
+
+  
+        <h4 id='brandstatement'>I have a passion for solving difficult problems and writing elegant code.</h4>
+      </div>
+
+
+</div>
       <img className='icon downarrow' src={process.env.PUBLIC_URL + '/images/down-arrow.png' } alt='icon' onClick={handleScroll}/>
+
     </div>
     <div>
       <Resume/>
