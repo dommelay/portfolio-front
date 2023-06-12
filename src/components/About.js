c53eaf60ee2d4f89d3fc83c32fb1309327c3af76
Dominique Clark <melaydominique@gmail.com>
2023-06-12 18:00:52 -0400

    almost done, Home.js responsive design

--- src/components/About.js
+++ src/components/About.js
@@ -25,11 +25,21 @@ const About = () => {
         </div>
 
 <div className='aboutcontainer'>
+        
+        <div className='abouttitle2'>
+        <h1 id='abouttitle2'>About</h1>
+        <a href='https://open.spotify.com/user/31sxqaklepkjjyscj3dl2lvs3buy' target='_blank'>
+                <img className='spoticon' src='/images/spotify.png' alt='Spotify'/>
+            </a>
 
-        <div>
+            <a href='https://www.instagram.com/dommelay/' target='_blank'>
+                <img className='igicon' src='/images/instagram.png'/>
+            </a>
+            <img className='leo' src='/images/leo.png' alt='Leo sign'/>
+        </div>
 
-            <img className='aboutpic' src='/images/portabout.jpg' alt='Dominique'/>
-
+        <div id='aboutpic'>
+            <img className='aboutpic' src='/images/fashionshot.jpg' alt='Dominique'/>
         </div>
 
     <div id='about'>
@@ -38,14 +48,23 @@ const About = () => {
         </div>
 
         <div className='igmusic'>
+
+            {/* href - target url
+            target - '_blank'
+            className - for styling
+            src - image pathway
+            alt - alternative text */}
+
             <a href='https://open.spotify.com/user/31sxqaklepkjjyscj3dl2lvs3buy' target='_blank'>
                 <img className='spoticon' src='/images/spotify.png' alt='Spotify'/>
             </a>
+
             <a href='https://www.instagram.com/dommelay/' target='_blank'>
                 <img className='igicon' src='/images/instagram.png'/>
             </a>
             <img className='leo' src='/images/leo.png' alt='Leo sign'/>
         </div>
+
         <div className='about'>
             <h2 className='bioab'>Beyond coding, I have a unique blend of passions and experiences. I am into fashion and I spent 2022 modeling in New York City. I do enjoy photoshoots and ocassionally runway, however I very much prefer to create my own content. Shoes are definitely the most important part of an outfit, and you will almost always catch me in sneakers.</h2><br/>
             <h2 className='bioab'>I am also an outspoken mental health advocate. I work to destigmatize mental health issues, especially those such as Bipolar Disorder and Schizophrenia. In support of my efforts, I have no shame sharing my own personal struggles. My university coursework and lived experienced make me expeceptionally knowledgeable in this field. I hope to push our society to deconstruct heavily biased Western psychiatry as it exists today.</h2><br/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -33,20 +33,22 @@ const Home = () => {
       <div id='titlediv'>
         <h1 id='name'>Dominique Clark</h1>
         <h2 id='title'>Software Engineer</h2>
+        <div className='brandstate' style={{margin: 0}}>
         <h4 id='brandstatement'>I am a Full-Stack Software Engineer with a background in Biopsychology, Neuroscience, and pre-med studies. My university course work has shaped me to be highly analytical and has helped me to develop excellent logic and reasoning skills. I am driven by complexity, and have a passion for solving difficult problems and writing elegant code. 
         </h4>
+        </div>
         <div id='icons'>
             <a href='https://github.com/dommelay' target='_blank'>
-                <img className='icon' src={process.env.PUBLIC_URL + '/images/github.png' } alt='icon'/>
-            </a>
-            <a href='mailto:melaydominique@gmail.com'>
-                <img className='icon' src={process.env.PUBLIC_URL + '/images/gmail.png' } alt='icon'/>
+                <img className='icon github' src={process.env.PUBLIC_URL + '/images/githubcolor.png' } alt='icon'/>
             </a>
             <a href='https://www.linkedin.com/in/dominiquemelayclark/' target='_blank'>
-            <img className='icon' src={process.env.PUBLIC_URL + '/images/linkedin.png' } alt='icon'/>
+            <img className='icon linkedin' src={process.env.PUBLIC_URL + '/images/linkedincolor.png' } alt='icon'/>
             </a>
             <a href='https://twitter.com/domsoftwaredev?lang=en' target='_blank'>
-                <img className='icon' src={process.env.PUBLIC_URL + '/images/twitter.png' } alt='icon'/>
+                <img className='icon twitter' src={process.env.PUBLIC_URL + '/images/twittercolor.png' } alt='icon'/>
+            </a>
+            <a href='mailto:melaydominique@gmail.com'>
+                <img className='icon gmail' src={process.env.PUBLIC_URL + '/images/gmailcolor.png' } alt='icon'/>
             </a>
         </div>
 
